06b18ff43d533159c27e83008f5429e713e2a382
JoasperScottan <78002056+Joasper@users.noreply.github.com>
2024-01-08 12:38:31 -0400

    Update Login-Controller.js

--- controllers/Login-Controller.js
+++ controllers/Login-Controller.js
@@ -17,14 +17,7 @@ const ObtenerUsuarios = async (req = request, res = response) => {
 const CrearUsuario = async (req = request, res = response) => {
   const { Billetera, Nombre, Apellido, Pais, Telefono } = req.body;
 
-  const validarBilletera = await Login.findOne({
-    Billetera: Billetera,
-  });
-  if (validarBilletera) {
-    return res.status(401).json({
-      msg: "Correo o Billetera ya existe",
-    });
-  }
+
 
   try {
     const login = new Login(req.body);
